cf3d3a9d1666fb03f4f769d415accf32479a9ef2
QTruonggg <quangtruong102000@gmail.com>
2022-10-03 18:06:12 +0700

    btvn

---
+++ src/javafx/Controllerrr.java
@@ -0,0 +1,39 @@
+package javafx;
+
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Controllerrr {
+    public TextField txtName;
+    public TextField txtPhone;
+
+    public Text namephone;
+    public Text namephone2;
+
+
+    public Text noticeName;
+    public Text noticePhone;
+
+
+    public void handleSubmit(){
+        String n = txtName.getText();
+        if(n.isEmpty()){
+            noticeName.setText("Vui lòng nhập tên người dùng");
+            noticeName.setVisible(true);
+        }else {
+            namephone.setText(n);
+            noticeName.setVisible(false);
+        }
+        String p = txtPhone.getText();
+        if(p.isEmpty()){
+            noticePhone.setText("Vui lòng nhập SĐT người dùng");
+            noticePhone.setVisible(true);
+        }else {
+            namephone.setText(p);
+            noticePhone.setVisible(false);
+        }
+        namephone.setText("1. " +n+ " - " +p);
+    }
+
+
+}
